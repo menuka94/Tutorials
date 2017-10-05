b4da3c3cf1a3aa768c5d927f24598fce34ba7643
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2017-10-05 23:30:41 +0530

    Spring:: feat: Implement RESTful Web Service

---
+++ Spring/RESTfulWebService/src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by menuka on 10/5/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
